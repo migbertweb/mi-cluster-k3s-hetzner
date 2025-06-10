6185b5bbef0e7571bd4ff4a66861676979b26c3e
migbertweb <migbertyanez@disroot.org>
2025-06-10 18:23:14 -0300

    deploycicd-php-redis

---
+++ apps/deployCICD/app/index.php
@@ -0,0 +1,8 @@
+<?php
+$redis = new Redis();
+$redis->connect('redis.default.svc.cluster.local', 6379);
+
+$visits = $redis->incr('visits');
+
+echo "<h1>Hola desde PHP en Kubernetes</h1>";
+echo "<p>Visitas: $visits</p>";
