f0b33e657434df89c5fa7b63a2a1fa6e55c4cbd1
migbertweb <migbertyanez@disroot.org>
2025-06-10 22:56:02 -0300

    mejor telegram

--- apps/deployCICD/app/index.php
+++ apps/deployCICD/app/index.php
@@ -3,6 +3,6 @@ $redis = new Redis();
 $redis->connect('redis.default.svc.cluster.local', 6379);
 
 $visits = $redis->incr('visits');
-// proba de novo - edit 2 no subi el cambio antes, otra vez equivoque el archivo
+// proba de novo - mejorando 
 echo "<h1>Hola desde PHP en Kubernetes</h1>";
 echo "<p>Visitas: $visits</p>";
