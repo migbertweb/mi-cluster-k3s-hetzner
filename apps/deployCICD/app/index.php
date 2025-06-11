de5c00d91b543fb990a5acb1f2ad4399197110d4
Migbert Yanez <migbertyanez@disroot.org>
2025-06-10 21:35:48 -0300

    workflow-edit

--- apps/deployCICD/app/index.php
+++ apps/deployCICD/app/index.php
@@ -3,6 +3,6 @@ $redis = new Redis();
 $redis->connect('redis.default.svc.cluster.local', 6379);
 
 $visits = $redis->incr('visits');
-// proba de novo
+// proba de novo - otra vez
 echo "<h1>Hola desde PHP en Kubernetes</h1>";
 echo "<p>Visitas: $visits</p>";
