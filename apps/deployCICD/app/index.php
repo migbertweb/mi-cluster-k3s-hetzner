6c454e10d501eb6a1d028594dc5e88cf7f46917d
migbertweb <migbertyanez@disroot.org>
2025-06-10 23:52:18 -0300

    quite el commit

--- apps/deployCICD/app/index.php
+++ apps/deployCICD/app/index.php
@@ -1,8 +1,8 @@
 <?php
 $redis = new Redis();
 $redis->connect('redis-service.redis.svc.cluster.local', 6379);
+$redis->auth('gpiro2178');
 
 $visits = $redis->incr('visits');
-// mejore el workflow2
 echo "<h1>Hola desde PHP en Kubernetes</h1>";
 echo "<p>Visitas: $visits</p>";
