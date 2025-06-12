cf0e3e45691742e73da7d884574f0bfdce7ad6d7
Migbert Yanez <migbertyanez@disroot.org>
2025-06-11 21:36:20 -0300

    feat: AÑADIDO DURACION FORMAT "minor"

--- apps/deployCICD/app/index.php
+++ apps/deployCICD/app/index.php
@@ -2,7 +2,7 @@
 $redis = new Redis();
 $redis->connect('redis-service.redis.svc.cluster.local', 6379);
 $redis->auth('gpiro2178');
-
+// CODIGO
 $visits = $redis->incr('visits');
 echo "<h1>Hola desde PHP en Kubernetes</h1>";
 echo "<p>Visitas: $visits</p>";
