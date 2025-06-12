1a400b7e6908c37a7ea2f5830b5dcf3afa7b9b23
Migbert Yanez <migbertyanez@disroot.org>
2025-06-11 21:50:44 -0300

    modificando la interfaz

--- apps/deployCICD/app/index.php
+++ apps/deployCICD/app/index.php
@@ -5,4 +5,12 @@ $redis->auth('gpiro2178');
 // CODIGO
 $visits = $redis->incr('visits');
 echo "<h1>Hola desde PHP en Kubernetes</h1>";
+echo "<p>Esta es una aplicación PHP desplegada en un clúster de Kubernetes.</p>";
+echo "<p>La fecha y hora actual es: " . date('Y-m-d H:i:s') . "</p>";
+echo "<p>El nombre del host es: " . gethostname() . "</p>";
+echo "<p>El nombre del contenedor es: " . getenv('HOSTNAME') . "</p>";
+echo "<p>El nombre del pod es: " . getenv('POD_NAME') . "</p>";
+echo "<p>El nombre del servicio es: " . getenv('SERVICE_NAME') . "</p>";
+echo "<p>El nombre del namespace es: " . getenv('NAMESPACE') . "</p>";
+
 echo "<p>Visitas: $visits</p>";
